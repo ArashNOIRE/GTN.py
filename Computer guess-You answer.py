53f69dfcafefeba343fb13c87c7451a199ebeea1
ASH <Arash.noire@outlook.com>
2025-03-16 23:16:58 +0330

    Add files via upload

--- Computer guess-You answer.py
+++ Computer guess-You answer.py
@@ -1,33 +1,46 @@
-#Requirements
+# Requirements
 import random as r
 #------------------------------------------------------------------
-#Variables
-maxguess = 100
-minguess = 1
-rnguess = r.randrange(minguess, maxguess)
-attempts = 0
-Bounes = 0
-#------------------------------------------------------------------
-#Guessing & Answering mechanism
-print (f"this is your number: {rnguess}")
-answer = input("my guess is right?")
-    #answer var must be '+' or '-' or '='
-while answer != "=":
-    if answer == "+":
-        minguess = rnguess
-        rnguess = r.randrange(minguess, maxguess)
-    elif answer == "-":
-        maxguess = rnguess
-        rnguess = r.randrange(minguess, maxguess)
-    print (f"my guess is {rnguess}")
+def main():  # The Main game code
+    # Variables
+    maxguess = 100
+    minguess = 1
+    rnguess = r.randrange(minguess, maxguess)
+    attempts = 0
+    bounes = 0
+    #------------------------------------------------------------------
+    # Guessing & Answering mechanism
+    print (f"this is your number: {rnguess}")
     answer = input("my guess is right?")
-    attempts = attempts + 1
-    bounes = 1
+        # ^^^answer must be + or - or = .^^^
+    while answer != "=":
+        if answer == "+":
+            minguess = rnguess
+            rnguess = r.randrange(minguess, maxguess)
+        elif answer == "-":
+            maxguess = rnguess
+            rnguess = r.randrange(minguess, maxguess)
+        print (f"my guess is {rnguess}")
+        answer = input("my guess is right?")
+        attempts = attempts + 1
+        bounes = 1
+    #------------------------------------------------------------------
+    # Finish Message
+    print ("Nice")
+    if bounes == 0:
+        print("WOW I guessed it without any Try!")
+    else:
+        print(f"I guessed it in {attempts} attempts.")
+# End of the game code
 #------------------------------------------------------------------
-#Finish Message
-print ("Nice")
-if bounes == 0:
-    print("WOW I guessed it without any Try!")
-else:
-    print(f"I guessed it in {attempts} attempts.")
+while True:  # Code runner
+    main()
+    again = input("Would you like to play again? (y/n): ")
+    if again == "y":
+        continue
+    else:
+        print("Thank you for playing")
+        break
+
+
 #Finish!
--- You guess-Computer answer.py
+++ You guess-Computer answer.py
@@ -1,30 +1,44 @@
-#Requirements
+# Requirements
 import random as r
 #------------------------------------------------------------------
-#Variables
-maxanswer = 100
-minanswer = 1
-answer = r.randint(1, 100)
-guess = (int(input("Guess a number between 1 and 100: ")))
-attempts = 0
-Bounes = 0
-#------------------------------------------------------------------
-#Guessing & Answering mechanism
-while answer != guess:
-    if answer > guess:
-        print("Too low")
-        minanswer = guess
+def main():  # The Main game code
+    # Variables
+    maxanswer = 100
+    minanswer = 1
+    answer = r.randint(1, 100)
+    guess = (int(input("Guess a number between 1 and 100: ")))
+    attempts = 0
+    bounes = 0
+    #------------------------------------------------------------------
+    # Guessing & Answering mechanism
+    while answer != guess:
+        if answer > guess:
+            print("My number is bigger")
+            minanswer = guess
+        else:
+            print("My number is smaller")
+            maxanswer = guess
+        guess = (int(input(f"Guess a number between {minanswer} and {maxanswer}: ")))
+        attempts = attempts + 1
+        bounes = 1
+    #------------------------------------------------------------------
+    # Finish Message
+    print("WELL DONE!!!")
+    if bounes == 0:
+        print("WOW you guessed without any Try")
     else:
-        print("Too high")
-        maxanswer = guess
-    guess = (int(input(f"Now guess a number between {minanswer} and {maxanswer}: ")))
-    attempts = attempts + 1
-    Bounes = 1
+        print(f"You guessed in {attempts} attempts")
+    print("Game finished")
+# End of the game code
 #------------------------------------------------------------------
-#Finish Message
-print("WELL DONE!!!")
-if Bounes == 0:
-    print("WOW you guessed without any Try")
-else:
-    print(f"You guessed in {attempts} attempts")
+while True:  # Code runner
+    main()
+    again = input("Would you like to play again? (y/n): ")
+    if again == "y":
+        continue
+    else:
+        print("Thank you for playing")
+        break
+
+
 #Finish!
