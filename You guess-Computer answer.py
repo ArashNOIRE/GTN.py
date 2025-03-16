d545d0d6b3f2a5322fa85d7308c1bc926b388848
ASH <Arash.noire@outlook.com>
2025-03-16 15:11:32 +0330

    Update You guess-Computer answer.py

--- You guess-Computer answer.py
+++ You guess-Computer answer.py
@@ -2,6 +2,8 @@
 import random as r
 #------------------------------------------------------------------
 #Variables
+maxanswer = 100
+minanswer = 1
 answer = r.randint(1, 100)
 guess = (int(input("Guess a number between 1 and 100: ")))
 attempts = 0
@@ -11,9 +13,11 @@ Bounes = 0
 while answer != guess:
     if answer > guess:
         print("Too low")
+        minanswer = guess
     else:
         print("Too high")
-    guess = (int(input("Guess a number between 1 and 100: ")))
+        maxanswer = guess
+    guess = (int(input(f"Guess a number between {minanswer} and {maxanswer}: ")))
     attempts = attempts + 1
     Bounes = 1
 #------------------------------------------------------------------
